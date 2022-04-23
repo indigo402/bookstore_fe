eeb14858d35bc2645d957dc2b5c5efd505ec6222
Indigo Noormuin <indigo.pasmer@hackermail.com>
2022-04-24 02:17:35 +0700

    upload project bookstore

---
+++ src/components/TabelBuku.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+function TabelBuku({ showEdit, books, requestToDelete }) {
+
+    function editData(book) {
+        showEdit(book);
+    }
+    
+    function deleteData(book) {
+        requestToDelete(book);
+    }
+    return (
+        <div>
+        <h4 className="mt-3">Tabel Data Buku</h4>
+        <table className="table table-bordered">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Judul</th>
+                <th>Pengarang</th>
+                <th className="col-3">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            {books.map((book, index) => (
+                <tr key={index}>
+                    <td> {index + 1}</td>
+                    <td> {book.judul}</td>
+                    <td> {book.pengarang}</td>
+                    <td>
+                        <button className="btn btn-info mx-2" onClick={() => editData(book)}>Edit</button>
+                        <button className="btn btn-danger" onClick={() => deleteData(book)}>Delete</button>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+    </div>
+    );
+}
+export default TabelBuku;
